fb7485b983bc3f8a595c20a53b1d2ede9d24fc43
Yoggi Firmanda <y.firmanda@netpoint-media.de>
2016-04-19 11:00:27 +0200

    create files
    + create chat
    + input script

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var express = require('express'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser');
-    var config = require('./config');
+
+var config = require('./config');
+// var engines = require('consolidate');
 
 var async = require('async');
 var cors = require('cors');
@@ -12,13 +14,78 @@ var cors = require('cors');
 var db = require('./model/db'),
     user = require('./model/users');
 
-var routes = require('./routes/index'),
-    users = require('./routes/users');
-
 var app = express();
+var http = require( "http" ).createServer( app );
+var io = require( "socket.io" )( http );
+
+// Chatroom
+var numUsers = 0;
+io.on('connection', function(socket){
+  console.log('socket');
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
+
+var routes = require('./routes/index'),
+    users = require('./routes/users'),
+    chats = require('./routes/chats')(io);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000);
@@ -31,6 +98,7 @@ app.use(cookieParser());
 
 app.use('/', routes);
 app.use('/user', users);
+app.use('/chat',chats);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -68,8 +136,11 @@ app.use(function(err, req, res, next) {
  | Start the Server
  |--------------------------------------------------------------------------
  */
-app.listen(app.get('port'), function() {
+http.listen(app.get('port'),function(){
   console.log('App express server listening on port ' + app.get('port'));
 });
+// app.listen(app.get('port'), function() {
+//   console.log('App express server listening on port ' + app.get('port'));
+// });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ var express = require('express'),
 
     //Any requests to this controller must pass through this 'use' function
     //Copy and pasted from method-override
-    router.use(bodyParser.urlencoded({ extended: true }))
+    router.use(bodyParser.urlencoded({ extended: false }))
     router.use(methodOverride(function(req, res){
           if (req.body && typeof req.body === 'object' && '_method' in req.body) {
             // look in urlencoded POST bodies and delete it
